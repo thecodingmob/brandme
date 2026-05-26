86fcb547c94cda66bab16cfa65f60ac32489d466
thecodingmob <thisismegoodness@gmail.com>
2026-05-26 13:00:06 +0100

    javascript for the hamburger and others.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ const nav = document.getElementById("navLinks");
 const menu = document.getElementById("menuToggle");
 
 menu.addEventListener("click", () => {
+  const icon = menu.querySelector("i");
+  icon.classList.toggle("fa-bars");
+  icon.classList.toggle("fa-times");
   nav.classList.toggle("open");
 });
