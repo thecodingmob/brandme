1ec4c8e74fae8bf32cc2df07ec7c651a31345c44
thecodingmob <thisismegoodness@gmail.com>
2026-06-09 13:55:19 +0100

    updated the script so that any part of the page can be clicked to close the nav bar

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
 const primaryNav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".menuToggle");
 
-navToggle.addEventListener("click", () => {
+// navToggle.addEventListener("click", () => {
+//   const visibility = primaryNav.getAttribute("data-visible");
+//   if (visibility === "false") {
+//     primaryNav.setAttribute("data-visible", true);
+//     navToggle.setAttribute("aria-expanded", true);
+//   } else if (visibility === "true") {
+//     primaryNav.setAttribute("data-visible", false);
+//     navToggle.setAttribute("aria-expanded", false);
+//   }
+// });
+
+function toggleMenu() {
   const visibility = primaryNav.getAttribute("data-visible");
 
   if (visibility === "false") {
     primaryNav.setAttribute("data-visible", true);
     navToggle.setAttribute("aria-expanded", true);
-  } else {
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+}
+
+navToggle.addEventListener("click", toggleMenu);
+
+document.addEventListener("click", (e) => {
+  if (!e.target.closest(".primary-navigation, .menuToggle")) {
     primaryNav.setAttribute("data-visible", false);
     navToggle.setAttribute("aria-expanded", false);
   }
